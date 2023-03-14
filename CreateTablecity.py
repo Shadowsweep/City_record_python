cf44cfd2435be21607fb4aa8aaf56e3c1ce4ae0d
Shadowsweep <122604770+Shadowsweep@users.noreply.github.com>
2023-03-14 07:53:53 +0530

    Update CreateTablecity.py

--- CreateTablecity.py
+++ CreateTablecity.py
@@ -1,6 +1,6 @@
 import pymysql as SQL
 try:
-    conn = SQL.connect(host = 'localhost',port = 3306,user='root',passwd='2022',database='Places')
+    conn = SQL.connect(host = 'localhost',port = 3306,user='root',passwd='2022',database='enter your database name')
     smt = conn.cursor()
     q = "create table Record( Placeid int primary key,Placename varchar(30),cityname varchar(30),statename varchar (30),PlaceDescription varchar(200),PlaceDistance varchar(30))"
 
